eeeb1f6a080d36aa208c9f3a588595a4c74af8cb
Bengie Villesco <bvillesco@gmail.com>
2024-12-18 17:51:10 +0800

    fix ui

--- obj/Debug/net9.0/pos_system.AssemblyInfo.cs
+++ obj/Debug/net9.0/pos_system.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("pos_system")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3b2eed32343b069a80a3120705e2ebbfe39aa5e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80f229c191c39677a8e4c7871bc369900f262eab")]
 [assembly: System.Reflection.AssemblyProductAttribute("pos_system")]
 [assembly: System.Reflection.AssemblyTitleAttribute("pos_system")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
