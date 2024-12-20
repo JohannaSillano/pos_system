ab88866715474eedf6e6ebb1fbee82fa5b7d544b
Bengie Villesco <bvillesco@gmail.com>
2024-12-20 22:19:21 +0800

    fix error: already migrated the database

--- obj/Debug/net9.0/pos_system.AssemblyInfo.cs
+++ obj/Debug/net9.0/pos_system.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("pos_system")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+160f5f8883b94a6a4813038a282b79ad0f58c4b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6b4f8dcc43a5899edc757cd5bfafc4a4b11ff1a")]
 [assembly: System.Reflection.AssemblyProductAttribute("pos_system")]
 [assembly: System.Reflection.AssemblyTitleAttribute("pos_system")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
