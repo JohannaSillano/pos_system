ab89f652da5baf94d49312cc0a2fb8f9d688041d
Edzra Macas <EdzraM7@gmail.com>
2025-01-08 22:33:03 +0800

    name

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -3,7 +3,8 @@ namespace pos_system.Models
     public class Employee
     {
         public int Id { get; set; }
-        public required string FullName { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
         public required string Email { get; set; }
         public required string Password { get; set; }
         public required string Role { get; set; }
--- obj/Debug/net9.0/pos_system.AssemblyInfo.cs
+++ obj/Debug/net9.0/pos_system.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("pos_system")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79669668803db4d462e77bae8b35ac21786dc833")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+631933c9517961e92538bce2a293af2d7fd1b3cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("pos_system")]
 [assembly: System.Reflection.AssemblyTitleAttribute("pos_system")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
