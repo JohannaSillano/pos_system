913db0dd2e8caa13ec60b09daaab977a9e5a4cb1
Edzra Macas <EdzraM7@gmail.com>
2025-01-08 23:42:59 +0800

    Fixed login

--- obj/Debug/net9.0/pos_system.AssemblyInfo.cs
+++ obj/Debug/net9.0/pos_system.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("pos_system")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+631933c9517961e92538bce2a293af2d7fd1b3cb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab89f652da5baf94d49312cc0a2fb8f9d688041d")]
 [assembly: System.Reflection.AssemblyProductAttribute("pos_system")]
 [assembly: System.Reflection.AssemblyTitleAttribute("pos_system")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
