b7cd491c7e1b5db6ef9c2476b108245976751f01
Bengie Villesco <bvillesco@gmail.com>
2025-01-09 15:31:44 +0800

    added 'searchProduct' function using HttpGet

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,6 +22,28 @@ namespace pos_system.Controllers
             var ProductList = _context.Products.Where(p => p.StockQuantity > 0).ToList();
             return View(ProductList);
         }
+
+        // Fetch product in search bar using GET
+        [HttpGet]
+        public JsonResult searchProduct(string productQuery)
+        {
+            if (string.IsNullOrEmpty(productQuery))
+            {
+                var ProductList = _context.Products
+                    .Where(p => !p.IsDeleted && p.StockQuantity > 0)
+                    .Select(p => new { p.Id, p.Name, p.Price, p.StockQuantity })
+                    .ToList();
+                return Json(ProductList); // Return all products
+            }
+            else
+            {
+                var matchingProducts = _context.Products
+                    .Where(p => !p.IsDeleted && p.StockQuantity > 0 && p.Name.ToLower().Contains(productQuery.ToLower()))
+                    .Select(p => new { p.Id, p.Name, p.Price, p.StockQuantity })
+                    .ToList();
+                return Json(matchingProducts); // Return matching products
+            }
+        }
     }
 }
 
