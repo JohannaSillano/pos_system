00caf0e763fe5c9c1fd4b89c4a6d1d3b8ac04191
Edzra Macas <EdzraM7@gmail.com>
2025-01-09 20:57:21 +0800

    .

--- obj/Debug/net9.0/pos_system.AssemblyInfo.cs
+++ obj/Debug/net9.0/pos_system.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("pos_system")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+215c56598588465e4dac17c26d0ede29cb0287b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d68fe412a9b31ed02f5bfe2939a99897d429178c")]
 [assembly: System.Reflection.AssemblyProductAttribute("pos_system")]
 [assembly: System.Reflection.AssemblyTitleAttribute("pos_system")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
