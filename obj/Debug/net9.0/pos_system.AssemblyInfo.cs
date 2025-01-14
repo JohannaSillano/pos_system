af2ec3cca68b1d70acfadb45ed432a7655e8fdf2
Edzra Macas <EdzraM7@gmail.com>
2025-01-15 03:28:09 +0800

    search transaction

--- obj/Debug/net9.0/pos_system.AssemblyInfo.cs
+++ obj/Debug/net9.0/pos_system.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("pos_system")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0535fd1a2084708c970e5bbc645c252cec161d23")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94b4da771ae4336b6b5d1598a700e582cff22cf7")]
 [assembly: System.Reflection.AssemblyProductAttribute("pos_system")]
 [assembly: System.Reflection.AssemblyTitleAttribute("pos_system")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
