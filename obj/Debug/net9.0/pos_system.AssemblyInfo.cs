ae171736bc70a292c75dcd35e7074de7b470aba7
Edzra Macas <EdzraM7@gmail.com>
2025-01-17 01:10:35 +0800

    fixed cart quantity change

--- obj/Debug/net9.0/pos_system.AssemblyInfo.cs
+++ obj/Debug/net9.0/pos_system.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("pos_system")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4c2ae3bcdbeaa3fe937f043d61f9ac3e8fc8794")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ff0bfce029521b8594e9738f9249e772416fa8c")]
 [assembly: System.Reflection.AssemblyProductAttribute("pos_system")]
 [assembly: System.Reflection.AssemblyTitleAttribute("pos_system")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
