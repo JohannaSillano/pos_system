6cdc9cffa699b2bfb3829b7637b0db05ddd2a7e4
Bengie Villesco <bvillesco@gmail.com>
2025-01-20 13:18:20 +0800

    push

--- obj/Debug/net9.0/pos_system.AssemblyInfo.cs
+++ obj/Debug/net9.0/pos_system.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("pos_system")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4767935d3fc740129020e2cbf17e13172d67cbd9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b29096553df6c3859930ccb357e39eb58921465")]
 [assembly: System.Reflection.AssemblyProductAttribute("pos_system")]
 [assembly: System.Reflection.AssemblyTitleAttribute("pos_system")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
